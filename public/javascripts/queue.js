acca8ab2b78a1c8c9c8c6e5ee46ab6607aa9d28a
Mark Hobson <markhobson@gmail.com>
2012-12-18 20:18:13 +0000

    Added queues view to client using backbone.js

---
+++ public/javascripts/queue.js
@@ -0,0 +1,54 @@
+// Queue View
+
+$(function() {
+	
+	var Queue = Backbone.Model.extend({
+		
+	});
+	
+	var Queues = Backbone.Collection.extend({
+		
+		url: "/queues",
+		
+		model: Queue
+		
+	});
+	
+	var QueueView = Backbone.View.extend({
+		
+		model: new Queue(),
+		
+		tagName: "li",
+		
+		template: _.template($("#queueTemplate").html()),
+			
+		render: function() {
+			this.$el.html(this.template(this.model.toJSON()));
+			return this;
+		}
+		
+	});
+	
+	var QueuesView = Backbone.View.extend({
+		
+		model: new Queues(),
+		
+		el: $("#queues"),
+		
+		initialize: function() {
+			this.model.bind("reset", this.render, this);
+		},
+		
+		render: function() {
+			_.each(this.model.models, function(item) {
+				var itemView = new QueueView({model: item});
+				this.$el.append(itemView.render().el);
+			}, this);
+			return this;
+		},
+		
+	});
+	
+	var queuesView = new QueuesView({model: new Queues()});
+	queuesView.model.fetch();
+});
