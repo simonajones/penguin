c0e420843d2ce5100a12dfa48fa841f3b8c26594
Mark Hobson <markhobson@gmail.com>
2012-12-20 23:02:58 +0000

    Removed jQuery on ready now scripts are at body end

--- public/js/queue.js
+++ public/js/queue.js
@@ -1,88 +1,84 @@
 // Queue View
 
-$(function() {
-	
-	// ------------------------------------------------------------------------
-	// Models
-	// ------------------------------------------------------------------------
-	
-	var pageModel = ko.mapping.fromJS({
-		section: "queuesView"
-	});
-	
-	pageModel.queuesView = {
-		
-		queues: ko.mapping.fromJS([]),
-		
-		show: function() {
-			$.getJSON("/api/queues", function(data) {
-				ko.mapping.fromJS(data, {}, pageModel.queuesView.queues);
-				pageModel.section("queuesView");
-			});
-		}
-	
-	};
-	
-	pageModel.queueView = {
-		
-		queue: ko.mapping.fromJS({
-			name: null
-		}),
-		
-		show: function(id) {
-			$.getJSON("/api/queue/" + id, function(data) {
-				ko.mapping.fromJS(data, {}, pageModel.queueView.queue);
-				pageModel.section("queueView");
-			});
-		}
-		
-	};
-	
-	pageModel.queueCreate = {
-		
-		queue: ko.mapping.fromJS({
-			name: null,
-		}),
-		
-		show: function() {
-			// TODO: reset model
-			pageModel.section("queueCreate");
-		},
-		
-		create: function() {
-			postJSON("/api/queues", ko.toJSON(this.queue), function() {
-				// TODO: show new queue
-				window.location.hash = "/queues";
-			});
-		},
-		
-	};
-	
-	ko.applyBindings(pageModel);
-	
-	// ------------------------------------------------------------------------
-	// Router
-	// ------------------------------------------------------------------------
-	
-	Router({
-		"/queues": pageModel.queuesView.show,
-		"/queue/new": pageModel.queueCreate.show,
-		"/queue/:id": pageModel.queueView.show
-	}).init("/queues");
+// ------------------------------------------------------------------------
+// Models
+// ------------------------------------------------------------------------
 
-	// ------------------------------------------------------------------------
-	// Utilities
-	// ------------------------------------------------------------------------
-	
-	// TODO: move elsewhere
-	var postJSON = function(url, data, success) {
-		return $.ajax({
-			type: "POST",
-			url: url,
-			contentType: "application/json",
-			data: data,
-			success: success
-		});
-	};
-	
+var pageModel = ko.mapping.fromJS({
+	section: "queuesView"
 });
+
+pageModel.queuesView = {
+	
+	queues: ko.mapping.fromJS([]),
+	
+	show: function() {
+		$.getJSON("/api/queues", function(data) {
+			ko.mapping.fromJS(data, {}, pageModel.queuesView.queues);
+			pageModel.section("queuesView");
+		});
+	}
+
+};
+
+pageModel.queueView = {
+	
+	queue: ko.mapping.fromJS({
+		name: null
+	}),
+	
+	show: function(id) {
+		$.getJSON("/api/queue/" + id, function(data) {
+			ko.mapping.fromJS(data, {}, pageModel.queueView.queue);
+			pageModel.section("queueView");
+		});
+	}
+	
+};
+
+pageModel.queueCreate = {
+	
+	queue: ko.mapping.fromJS({
+		name: null,
+	}),
+	
+	show: function() {
+		// TODO: reset model
+		pageModel.section("queueCreate");
+	},
+	
+	create: function() {
+		postJSON("/api/queues", ko.toJSON(this.queue), function() {
+			// TODO: show new queue
+			window.location.hash = "/queues";
+		});
+	},
+	
+};
+
+ko.applyBindings(pageModel);
+
+// ------------------------------------------------------------------------
+// Router
+// ------------------------------------------------------------------------
+
+Router({
+	"/queues": pageModel.queuesView.show,
+	"/queue/new": pageModel.queueCreate.show,
+	"/queue/:id": pageModel.queueView.show
+}).init("/queues");
+
+// ------------------------------------------------------------------------
+// Utilities
+// ------------------------------------------------------------------------
+
+// TODO: move elsewhere
+var postJSON = function(url, data, success) {
+	return $.ajax({
+		type: "POST",
+		url: url,
+		contentType: "application/json",
+		data: data,
+		success: success
+	});
+};
