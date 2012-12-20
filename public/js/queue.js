c29dc46d79b5bb74df58b2cbadf67bda581c0e15
Mark Hobson <markhobson@gmail.com>
2012-12-20 22:22:24 +0000

    Formatting

--- public/js/queue.js
+++ public/js/queue.js
@@ -11,39 +11,49 @@ $(function() {
 	});
 	
 	pageModel.queuesView = ko.mapping.fromJS({
+		
 		queues: [],
+		
 		show: function() {
 			$.getJSON("/api/queues", function(data) {
 				ko.mapping.fromJS(data, {}, pageModel.queuesView.queues);
 				pageModel.section("queuesView");
 			});
 		}
+	
 	});
 	
 	pageModel.queueView = ko.mapping.fromJS({
+		
 		queue: {
 			name: null
 		},
+		
 		show: function(id) {
 			$.getJSON("/api/queue/" + id, function(data) {
 				ko.mapping.fromJS(data, {}, pageModel.queueView.queue);
 				pageModel.section("queueView");
 			});
 		}
+		
 	});
 	
 	pageModel.queueCreate = ko.mapping.fromJS({
+		
 		queue: {
 			name: null,
 		},
+		
 		show: function() {
 			pageModel.section("queueCreate");
 		},
+		
 		create: function() {
 			postJSON("/api/queues", ko.toJSON(this.queue), function() {
 				// TODO: show new queue
 			});
 		}
+		
 	});
 	
 	ko.applyBindings(pageModel);
@@ -53,13 +63,9 @@ $(function() {
 	// ------------------------------------------------------------------------
 	
 	Router({
-		
 		"/queues": pageModel.queuesView.show,
-		
 		"/queue/new": pageModel.queueCreate.show,
-		
 		"/queue/:id": pageModel.queueView.show
-		
 	}).init("/queues");
 
 	// ------------------------------------------------------------------------
