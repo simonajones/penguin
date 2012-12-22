8a081adb9fe6258f60047e8ede39031fb3dbfe2c
Mark Hobson <markhobson@gmail.com>
2012-12-22 16:13:28 +0000

    Used protocol relative URLs for all external resources

--- public/js/client.js
+++ public/js/client.js
@@ -4,11 +4,11 @@
 require.config({
 	
 	paths: {
-		jquery: "http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min",
-		knockout: "http://ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.0",
+		jquery: "//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min",
+		knockout: "//ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.0",
 		knockoutmapping: "lib/knockout.mapping-latest",
 		director: "lib/director.min",
-		bootstrap: "http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min"
+		bootstrap: "//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min"
 	},
 	
 	shim: {
