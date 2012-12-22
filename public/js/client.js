6c24e21d8ba5e9fc2bb22084f0169ea8060de33c
Mark Hobson <markhobson@gmail.com>
2012-12-22 16:22:15 +0000

    Moved jquery-json into lib directory

--- public/js/client.js
+++ public/js/client.js
@@ -5,6 +5,7 @@ require.config({
 	
 	paths: {
 		jquery: "//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min",
+		"jquery-json": "lib/jquery-json",
 		knockout: "//ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.0",
 		knockoutmapping: "//cdnjs.cloudflare.com/ajax/libs/knockout.mapping/2.3.4/knockout.mapping",
 		director: "lib/director.min",
