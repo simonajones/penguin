a43b1eeb31c743eb9c0f7a0915a325d89f49c9e0
Mark Hobson <markhobson@gmail.com>
2012-12-22 14:13:47 +0000

    Moved router configuration to separate script

--- public/js/queue.js
+++ public/js/queue.js
@@ -5,13 +5,3 @@
 // ------------------------------------------------------------------------
 
 ko.applyBindings(page);
-
-// ------------------------------------------------------------------------
-// Router
-// ------------------------------------------------------------------------
-
-var router = Router();
-router.on("/queues", page.queuesView.show);
-router.on("/queue/new", page.queueCreate.show);
-router.on("/queue/:id", page.queueView.show);
-router.init("/queues");
---
+++ public/js/router.js
@@ -0,0 +1,8 @@
+/*
+ * Router.
+ */
+var router = Router();
+router.on("/queues", page.queuesView.show);
+router.on("/queue/new", page.queueCreate.show);
+router.on("/queue/:id", page.queueView.show);
+router.init("/queues");
