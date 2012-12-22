a39cb9b5799c750bae2af0777978ec5b8af0ee85
Mark Hobson <markhobson@gmail.com>
2012-12-22 14:24:13 +0000

    Formatting

--- public/js/router.js
+++ public/js/router.js
@@ -2,7 +2,11 @@
  * Router.
  */
 var router = Router({
+	
 	"/queues": page.queuesView.show,
+	
 	"/queue/new": page.queueCreate.show,
+	
 	"/queue/:id": page.queueView.show
+	
 }).init("/queues");
