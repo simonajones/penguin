07f366ec45c61c13656abe2c14427c129cc2a3c8
Mark Hobson <markhobson@gmail.com>
2012-12-22 17:32:39 +0000

    Tweaked log message

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.post("/api/queues", queue.create);
 
 http.createServer(app).listen(app.get("port"), function()
 {
-	console.log("Express server listening on port " + app.get("port"));
+	console.log("Server listening on port " + app.get("port"));
 });
